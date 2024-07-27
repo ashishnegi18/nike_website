98291512bc70cb999897d43935b0ee7d0f37ed34
ashu0831 <ashunegi0831@gmail.com>
2024-07-27 11:46:26 +0530

    Add .gitattributes to manage line endings

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import Services from "./Services";
+import CustomerReviews from "./CustomerReviews";
+import PopularProducts from "./PopularProducts";
+import SpecialOffers from "./SpecialOffers";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Hero,
+  Footer,
+  Services,
+  CustomerReviews,
+  PopularProducts,
+  SpecialOffers,
+  Subscribe,
+  SuperQuality,
+};
+
